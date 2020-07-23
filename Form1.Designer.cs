b17169c1e2c172fdfed1903ee9dd168b08ab1323
AnthonyMarsili <56132966+AnthonyMarsili@users.noreply.github.com>
2020-07-23 15:36:03 -0400

    almost there...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // ConvertButton
             // 
-            this.ConvertButton.Location = new System.Drawing.Point(362, 405);
+            this.ConvertButton.Location = new System.Drawing.Point(601, 506);
+            this.ConvertButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ConvertButton.Name = "ConvertButton";
-            this.ConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertButton.Size = new System.Drawing.Size(84, 29);
             this.ConvertButton.TabIndex = 0;
             this.ConvertButton.Text = "Convert";
             this.ConvertButton.UseVisualStyleBackColor = true;
@@ -48,53 +49,56 @@
             // InputBox
             // 
             this.InputBox.AcceptsReturn = true;
-            this.InputBox.Location = new System.Drawing.Point(30, 32);
+            this.InputBox.Location = new System.Drawing.Point(34, 40);
+            this.InputBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.InputBox.Multiline = true;
             this.InputBox.Name = "InputBox";
             this.InputBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.InputBox.Size = new System.Drawing.Size(370, 356);
+            this.InputBox.Size = new System.Drawing.Size(600, 444);
             this.InputBox.TabIndex = 1;
             // 
             // ASNLabel
             // 
             this.ASNLabel.AutoSize = true;
-            this.ASNLabel.Location = new System.Drawing.Point(175, 9);
+            this.ASNLabel.Location = new System.Drawing.Point(296, 9);
             this.ASNLabel.Name = "ASNLabel";
-            this.ASNLabel.Size = new System.Drawing.Size(36, 17);
+            this.ASNLabel.Size = new System.Drawing.Size(42, 20);
             this.ASNLabel.TabIndex = 2;
             this.ASNLabel.Text = "ASN";
             // 
             // OutputBox
             // 
             this.OutputBox.AcceptsReturn = true;
-            this.OutputBox.Cursor = System.Windows.Forms.Cursors.No;
-            this.OutputBox.Location = new System.Drawing.Point(421, 32);
+            this.OutputBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.OutputBox.Location = new System.Drawing.Point(650, 40);
+            this.OutputBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.OutputBox.Multiline = true;
             this.OutputBox.Name = "OutputBox";
             this.OutputBox.ReadOnly = true;
             this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.OutputBox.Size = new System.Drawing.Size(367, 356);
+            this.OutputBox.Size = new System.Drawing.Size(600, 444);
             this.OutputBox.TabIndex = 3;
             // 
             // AdviceLabel
             // 
             this.AdviceLabel.AutoSize = true;
-            this.AdviceLabel.Location = new System.Drawing.Point(557, 9);
+            this.AdviceLabel.Location = new System.Drawing.Point(891, 9);
             this.AdviceLabel.Name = "AdviceLabel";
-            this.AdviceLabel.Size = new System.Drawing.Size(116, 17);
+            this.AdviceLabel.Size = new System.Drawing.Size(129, 20);
             this.AdviceLabel.TabIndex = 4;
             this.AdviceLabel.Text = "Receiving Advice";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1278, 562);
             this.Controls.Add(this.AdviceLabel);
             this.Controls.Add(this.OutputBox);
             this.Controls.Add(this.ASNLabel);
             this.Controls.Add(this.InputBox);
             this.Controls.Add(this.ConvertButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -23,9 +23,6 @@ namespace EDIFileGenerator
             public static String outputText = "";
         }
 
-
-
-
         private List<String> WholeTextParse(String text)
         {
             List<String> words = new List<String>();
@@ -46,7 +43,7 @@ namespace EDIFileGenerator
                     words.Add(currentWord);
                     currentWord = "";
                 }
-                else if (currentChar == "~")
+                else if (currentChar == "~" || currentChar == "`")
                 {
                     words.Add(currentWord);
                     words.Add("~");
@@ -63,14 +60,14 @@ namespace EDIFileGenerator
             return words;
         }
 
-        private void PutBackTogether(List<String> inputList) {
+        private String PutBackTogether(List<String> inputList) {
             //Put all the words back together with proper delimiters 
             String output = "";
             int counter = 0;
             int length = inputList.Count;
             while (counter < length)
             {
-                if (inputList[1] == "~")
+                if (inputList[1] == "~" || inputList[1] == "`")
                 {
                     output += inputList[0] + "~\r\n";
                     inputList.RemoveAt(0);
@@ -88,38 +85,88 @@ namespace EDIFileGenerator
 
             }
 
-            Globals.outputText += output;
+            return output;
 
         }
 
         private void TopEnvelopeFlipper(List<String> inEnvelope) {
+            // swaps sender and receiver on ISA and GS
             String temp = inEnvelope[6];
             inEnvelope[6] = inEnvelope[8];
             inEnvelope[8] = temp;
-
             temp = inEnvelope[20];
             inEnvelope[20] = inEnvelope[21];
             inEnvelope[21] = temp;
 
-            PutBackTogether(inEnvelope);
+            // gets current date and puts in ISA and GS
+            String today = DateTime.Now.ToString("yyMMdd");
+            inEnvelope[9] = today;
+            inEnvelope[22] = "20" + today;
 
-            //First 6 segments stay the same
-            //for (int i = 0; i < inEnvLen; i++)
-            //{
-            //    if (i == 6) // these two if statements do the swapping
-            //        outEnvelope += reciever;
-            //    else if (i == 8)
-            //        outEnvelope += sender;
-            //    else
-            //        outEnvelope += inEnvelope[0];
-                
-            //    outEnvelope += "*";
-            //    inEnvelope.RemoveAt(0);
-            //}
+            // gets current time and puts in ISA and GS
+            String time = DateTime.Now.ToString("hhmm");
+            inEnvelope[10] = time;
+            inEnvelope[23] = time;
+
+            // makes the output be an RC
+            // HARDCODED for now but will change based on user dropdown input
+            inEnvelope[19] = "RC";
+            inEnvelope[29] = "861";
+
+            Globals.outputText += PutBackTogether(inEnvelope);
+        }
+
+        private void ReceivingAdviceMaker(List<String> segments) {
+            int count = 0;
+            String asnNum = segments[2];
+
+            // builds the BRA
+            Globals.outputText += "BRA*" + asnNum + 
+                "*20" + DateTime.Now.ToString("yyMMdd") + "*06*8~\r\n";
+            // builds the REF
+            Globals.outputText += "REF*SI*" + asnNum + "~\r\n";
+            // builds the DTM
+            Globals.outputText += "DTM*999" + "*20" + DateTime.Now.ToString("yyMMdd") + "~\r\n";
+
+            //loop through segments list
+            // delete everything until you get to a tilde
+            // if the next string is not PRF, delete everything until you get to a tilde
+            // if it is PRF, grab segments[1] and that is your PO number
+            // then delete that PRF and everything until you get to a tilde
+            // if the next string is not LIN, keep deleting
+            // if it is, exit loop
+
+            int lineCount = 0;
+            String tempLIN;
+            String tempQty;
+            String poNum = "";
+            while (segments[0] != "CTT") {
+                if (segments[0] == "~")
+                {
+                    if (segments[1] == "PRF") {
+                        poNum = segments[2];
+                    }
+                    else if (segments[1] == "LIN") {
+                        lineCount++;
+                        tempLIN = PutBackTogether(segments.GetRange(1, 7));
+                        tempQty = segments[9];
+                        Globals.outputText += "RCD**" + tempQty + "*ST~\r\n" + tempLIN + "PRF*" + poNum + "~\r\n";
+                    }
+                    segments.RemoveAt(0);
+                }
+                else {
+                    segments.RemoveAt(0);
+                }
+            }
+
+            // now segments starts at first LIN and we have the po Num
+            
         }
 
         private void ConvertButton_Click(object sender, EventArgs e)
         {
+            //OutputBox.Text = "";
+            Globals.outputText = "";
             //InputBox.AppendText("Test");
             String inputText = "";
             String outputText = "";
@@ -129,17 +176,22 @@ namespace EDIFileGenerator
             int counter = 0;
             
             inputText = InputBox.Text;
-            length = inputText.Length;
 
             //Split all the words based on the value "|" or "*"
             words = WholeTextParse(inputText);
            
-            //Change word count to be the size of the envelope
-            for (int i = 0; i < words.Count; i++) {
-                envelope.Add(words[i]);
+            // 32 is the size of the envelope
+            for (int i = 0; i < 32; i++) 
+            {
+                envelope.Add(words[0]);
+                words.RemoveAt(0);
             }
 
             TopEnvelopeFlipper(envelope);
+
+            // now "words" contains everything after the top envelope
+
+            ReceivingAdviceMaker(words);
 
             OutputBox.Text = Globals.outputText;
         }
